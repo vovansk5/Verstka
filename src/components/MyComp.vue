fb9946c8eba9e96d29e3d79c64d702ff5600cd5a
vovansk5 <vovansk5@gmail.com>
2020-05-12 09:38:57 +0700

    fix

---
+++ src/components/MyComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+      Это тест из MyComp2
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
