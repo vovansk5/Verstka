02b6746c3ad2ad3b9076da8343764c65f659b8a3
vovansk5 <vovansk5@gmail.com>
2020-05-13 12:10:54 +0700

    13

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,43 @@
 <template>
   <div id="app">
     <MyComp msg="Welcome to Your Vue.js App"/>
+
+    <fieldset>
+        <legend>Главное</legend>
+        
+        <input v-model="firstName" placeholder="Фамилия">
+        <br>
+        <select v-model="city" multiple>
+          <option value="1">Новосибирск</option>
+          <option value="2">Барнаул</option>
+          <option value="3">Томск</option>
+          <option value="4">Москва</option>
+        </select>
+        
+        <br>
+        
+        <img v-bind:src="imageSrc">
+        {{imageSrc}}
+        <img src="/MyMedia/1.jpg">
+
+        <br>
+        
+        <audio v-bind:src="mySound" controls >Послушай меня</audio>
+        <audio v-if="numSong==1" src='./assets/appl.mp3' controls >Апплодисменты</audio>
+        <audio v-else src='./assets/steps.mp3' controls >Шаги</audio>
+        
+
+        <button @click="mySound='/MyMedia/appl.mp3'"> Апплодисменты </button>
+        <button @click="mySound='/MyMedia/steps.mp3'"> Шаги </button>
+        <button @click="numSongCalc">Следующая песня</button>
+
+        <br>
+        Музыка={{mySound}}
+
+    </fieldset>
+
+
+
   </div>
 </template>
 
@@ -9,6 +46,19 @@ import MyComp from './components/MyComp.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+    firstName:'Петров',
+    city:2,
+    imageSrc:"/MyMedia/1.jpg",
+    mySound:'./assets/appl.mp3',
+    numSong:1}
+  },
+  methods: {
+    numSongCalc() {
+      return (this.numSong==2) ? this.numSong=1 : this.numSong=2
+    }
+  },
   components: {
     MyComp
   }
---
+++ src/components/MyForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="myForm">
+    Это компонент форма
+    <form name="myForm" action="Get">
+      <label> Сок </label>
+      <input name="juse" type="checkbox" value="1"> Яблочный
+      <input name="juse" type="checkbox" value="1"> Вишневый
+      <input name="juse" type="checkbox" value="1"> Грушевый
+      <br>
+
+
+      <label>файл <input type="file" name="file1"></label>
+
+      <br>  
+      <label for="animals">Животное </label>
+      <select id="animals" name="an" multiple size="3">
+        <optgroup label="Крупные">
+          <option value="1">Медведь</option>
+        </optgroup>
+        <option value="2" selected >Заяц</option>
+        <option value="3">Лев</option>
+      </select>
+
+      <br>  
+      <input type="hidden" name="hid" value="100" >
+
+      <br>
+      <input type="submit" value="send">
+      
+
+      
+    </form>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'myForm',
+    props: {
+        city: String
+    }
+}
+</script>
