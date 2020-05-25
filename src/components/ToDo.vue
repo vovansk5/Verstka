8f7fcf5e00dad99a3e896cf83bb14f66d0551bfe
vovansk5 <vovansk5@gmail.com>
2020-05-25 15:25:20 +0700

    aasdf

---
+++ src/components/ToDo.vue
@@ -0,0 +1,14 @@
+<template>
+    <ul>
+        <li v-for="item in todos" key="item">
+            {{item}}
+            
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name:'MyToDO'
+}
+</script>
